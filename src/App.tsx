c9087d1df4145c25a71621abf362bd75d16809cb
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-10-25 09:08:25 +0300

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <section className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-7xl font-bold">App</h1>
+    </section>
+  );
+};
