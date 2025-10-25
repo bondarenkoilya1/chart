0f007a591337dee5ee3af9edd40ffd9d1e94bc76
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-10-25 09:31:42 +0300

    add grid and x axis

--- src/components/StatusChart/StatusChart.tsx
+++ src/components/StatusChart/StatusChart.tsx
@@ -1,15 +1,34 @@
 import { ChartContainer } from "@/components/ui/chart.js";
 import { chartConfig } from "@/components/StatusChart/config.js";
-import { AreaChart, Area } from "recharts";
+import { AreaChart, Area, CartesianGrid, XAxis } from "recharts";
 import { chartData } from "@/components/StatusChart/data.js";
 
 export const StatusChart = () => {
+  const dataKeys = ["desktop", "mobile"];
+
   return (
     <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
       <AreaChart accessibilityLayer data={chartData}>
-        <Area dataKey="desktop" fill="var(--color-desktop)" type="natural" fillOpacity={0.4} />
-        <Area dataKey="mobile" fill="var(--color-mobile)" type="natural" fillOpacity={0.4} />
+        <GridAndAxis />
+        {dataKeys.map((key) => (
+          <Area dataKey={key} fill={`var(--color-${key})`} type="natural" fillOpacity={0.4} />
+        ))}
       </AreaChart>
     </ChartContainer>
   );
 };
+
+function GridAndAxis() {
+  return (
+    <>
+      <CartesianGrid vertical={false} />
+      <XAxis
+        dataKey="month"
+        tickLine={false}
+        tickMargin={10}
+        axisLine={false}
+        tickFormatter={(value) => value.slice(0, 3)}
+      />
+    </>
+  );
+}
